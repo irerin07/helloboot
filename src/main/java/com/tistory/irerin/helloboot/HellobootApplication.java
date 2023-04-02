d0eafb798b5a1e4ce6edbd9eddec9354f838ac72
irerin07 <counterchord89@gmail.com>
2023-04-02 21:08:00 +0900

    토비 스프링 부트 3챕터 간단한 서블릿

--- src/main/java/com/tistory/irerin/helloboot/HellobootApplication.java
+++ src/main/java/com/tistory/irerin/helloboot/HellobootApplication.java
@@ -1,10 +1,19 @@
 package com.tistory.irerin.helloboot;
 
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
 @SpringBootApplication
 public class HellobootApplication {
@@ -12,7 +21,19 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+
+			}).addMapping("/hello");
+
+		});
+
 		webServer.start();
 
 	}
