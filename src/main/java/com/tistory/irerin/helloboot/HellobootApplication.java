48810bb0197d143002b12bfd4352ec353725c8d0
irerin07 <counterchord89@gmail.com>
2023-04-02 21:33:07 +0900

    토비 스프링 부트 3챕터 프런트 컨트롤러

--- src/main/java/com/tistory/irerin/helloboot/HellobootApplication.java
+++ src/main/java/com/tistory/irerin/helloboot/HellobootApplication.java
@@ -14,6 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @SpringBootApplication
 public class HellobootApplication {
@@ -22,20 +26,28 @@ public class HellobootApplication {
 
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Content-Type", "text/plain");
-					resp.getWriter().println("Hello Servlet");
+					//인증, 보안, 다국어등의 공통 기능 처리
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello Servlet" + name);
+					} else if (req.getRequestURI().equals("/user")) {
+						// do something
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
 
-			}).addMapping("/hello");
+			}).addMapping("/*");
 
 		});
 
 		webServer.start();
-
 	}
 
 }
